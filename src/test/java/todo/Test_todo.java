d9c00920c74ff193b366ac88f3c3ebe55a748dce
Esberfes <casas222@hotmail.com>
2018-04-27 12:53:40 +0200

    Init config Pom.xml
    Maven-compiler
    J-Connector
    JUnit

---
+++ src/test/java/todo/Test_todo.java
@@ -0,0 +1,17 @@
+package todo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_todo {
+
+	@Test
+	public void test() {
+		int a = 2;
+		int b = 1;
+		
+		assertEquals(5, a+b);
+	}
+
+}
